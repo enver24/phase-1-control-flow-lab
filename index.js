7cb7f490bc73f3a8a608df47efa7b156353c72d8
Enver <mr_epsty@hotmail.com>
2023-10-29 17:43:38 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
+  }
+  else if (feet > 400 && feet < 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (feet > 2000 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  switch (city) {
+    case 'NYC':
+      return 'Ok, sounds good.'
+      break;
+    default:
+      return 'No go.'
+      break;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break
+      case 'not as generous':
+        return 'Thank you.'
+    default:
+      return 'Bye.'
+      break
+  }
 }
